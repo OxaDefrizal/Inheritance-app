fc64a20e1363288a38ed7165dd5137be2ee9a195
OxaDefrizal <oxadefrizal@students.amikom.ac.id>
2022-06-10 09:59:36 +0700

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectInheritance
+{
+    public class Person 
+    {
+        public Person(string nama, int age)
+        {
+            this.Name = nama;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        
+        public void GetNameAndAge()
+        {
+            Console.WriteLine("The person has name: {0} and age: {1}", Name, Age);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectInheritance
+{
+    public class Student : Person
+    {
+        public Student(string Name, int Age, string studentid, string email)
+        : base(Name, Age)
+        {
+            this.StudentId = studentid;
+            this.Email = email;
+        }
+        public string StudentId { get; set; }
+        public string Email { get; set; }
+    }
+}
